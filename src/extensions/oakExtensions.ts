75de21d91e857379258f7361198c29f062e8f812
Joakim Carlsson <JoakimCarlsson1994@gmail.com>
2024-01-31 21:56:28 +0100

    .

--- src/extensions/oakExtensions.ts
+++ src/extensions/oakExtensions.ts
@@ -1,12 +1,12 @@
 ﻿import { Context } from "https://deno.land/x/oak@v13.0.0/mod.ts";
 
-Context.prototype.toJsonResponse = function (this: Context, statusCode: number, messageOrBody: string | object): void {
+Context.prototype.toJsonResponse = function (this: Context, statusCode: number, json: string | object): void {
     this.response.type = "application/json";
     this.response.status = statusCode;
 
-    if (typeof messageOrBody === 'string') {
-        this.response.body = { message: messageOrBody };
+    if (typeof json === 'string') {
+        this.response.body = { message: json };
     } else {
-        this.response.body = messageOrBody;
+        this.response.body = json;
     }
 };
