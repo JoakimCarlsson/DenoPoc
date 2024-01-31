a4acf6b089fed00afe93e61bcfc7fc97d42e9bd6
Joakim Carlsson <JoakimCarlsson1994@gmail.com>
2024-01-31 21:55:24 +0100

    renamed jsOrObj => json

--- src/extensions/oak-augmentation.d.ts
+++ src/extensions/oak-augmentation.d.ts
@@ -1,8 +1,9 @@
-﻿import { Context } from "https://deno.land/x/oak@v13.0.0/mod.ts"
+﻿// deno-lint-ignore no-unused-vars
+import { Context } from "https://deno.land/x/oak@v13.0.0/mod.ts"
 
 declare module "https://deno.land/x/oak@v13.0.0/mod.ts" {
     interface Context {
-        toJsonResponse: (statusCode: number, jsOrObj: string | object) => void;
+        toJsonResponse: (statusCode: number, json: string | object) => void;
     }
 }
 
