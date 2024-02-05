530fb26bc22c9200ab06f74debc7239fb5ef5151
Joakim Carlsson <joakim.carlsson1994@gmail.com>
2024-02-05 15:59:39 +0100

    wip.

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,26 @@
-import { Application, Router } from "https://deno.land/x/oak@v13.0.0/mod.ts";
-import { authMiddleware } from "./common/middlewares/authMiddleware.ts";
+import {
+    Application,
+    Context,
+    Router,
+} from "https://deno.land/x/oak@v12.6.2/mod.ts";
 
 const app = new Application();
 const router = new Router();
 
-router.get("/", (context) => {
-  context.response.body = "Hello, World!";
+router.post("/", async (context: Context) => {
+    if (context.request.hasBody) {
+        const result = context.request.body();
+        if (result.type === "form-data") {
+            const formData = await result.value.read();
+            console.log(formData);
+        }
+    } else {
+        context.response.status = 400; // Bad Request
+        context.response.body = "No body found";
+    }
 });
-
-app.use(authMiddleware);
 
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-await app.listen({ port: 8000 });
+await app.listen({port: 9907});
